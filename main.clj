1628564affe8e0423e9fed72afb85e0b01e2a399
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-03 15:12:16 +0000

    Gain Clojure powers

--- main.clj
+++ main.clj
@@ -1,1 +1,25 @@
-(println "Hello World!")
+(ns clojure.examples.aoc22day3
+   (:gen-class))
+
+(require '[clojure.string :as str])
+(require '[clojure.java.io :as jio])
+
+(defn FirstHalf [line] 
+    (let [len (count line)] 
+        (subs line 0 (/ len 2))))
+
+(defn SecondHalf [line] 
+    (let [len (count line)] 
+        (subs line (/ len 2))))
+
+;; Read all the lines one by one
+(defn ReadLines []
+(with-open [rdr (jio/reader "Example.txt")]
+  (doseq [line (line-seq rdr)]
+    (println 
+     (str 
+      (FirstHalf line)
+      "-"
+      (SecondHalf line)
+      )))))
+(ReadLines)
