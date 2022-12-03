bcb2ba941097a441fb84fb0d3b97e4bfd4edd95f
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-03 16:56:21 +0000

    Experiment

--- main.clj
+++ main.clj
@@ -12,30 +12,53 @@
     (let [len (count line)] 
         (subs line (/ len 2))))
 
-(defn packIntersection [line]
+(defn PackIntersection [line]
   (set/intersection (set (FirstHalf line)) (set (SecondHalf line))))
 
-(defn charScore [c]     
+(defn CharScore [c]     
     (println c)
     (let [intVal (int c)] 
         (if (<= intVal (int \Z))
             (+ (- intVal (int \A)) 27)
             (+ (- intVal (int \a)) 1))))
 
-(defn packScore [line] 
-    (let [intersectSet (packIntersection line)]
-        (reduce + (map charScore intersectSet))
+(defn PackScore [line] 
+    (let [intersectSet (PackIntersection line)]
+        (reduce + (map CharScore intersectSet))
         ))
 
 (defn ReadLines []
     (with-open [rdr (jio/reader "Example.txt")]
       (doseq [line (line-seq rdr)]
           (println (str ">" line "<"))
-          (println (str "= " (packScore line)))
+          (println (str "= " (PackScore line)))
         )))
 
 (defn CalcLines []
-    (with-open [rdr (jio/reader "Input.txt")] (println (reduce + (map packScore (line-seq rdr))))))
+    (with-open [rdr (jio/reader "Input.txt")] 
+        (println (reduce + (map PackScore (line-seq rdr))))))
 
-(ReadLines)
-(CalcLines)
+(defn FindBadge 
+    ([line1 line2 line3] 
+         (println (set/intersection (set line1) (set line2) (set line3))))
+    ([lines]
+        (FindBadge (first lines) (first (next lines)) (first (next (next lines))) )))
+
+;; (ReadLines)
+;; (CalcLines)
+
+(defn FindBadges []
+    (with-open [rdr (jio/reader "Input.txt")] 
+        (doseq [part (partition 3 (line-seq rdr))] 
+            (FindBadge part) )))
+
+(defn CalcBadges []
+    (with-open [rdr (jio/reader "Input.txt")] 
+        (doseq [part (partition 3 (line-seq rdr))] 
+            (FindBadge part) )))
+
+(defn CalcBadges2 []
+    (with-open [rdr (jio/reader "Input.txt")] 
+        (println (reduce + (map PackScore (FindBadge (partition 3 (line-seq rdr))))))))
+
+(CalcBadges2)
